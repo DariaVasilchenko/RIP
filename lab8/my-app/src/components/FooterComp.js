210c13fea80c9f8fd77fcaa6cd790d54fe48abae
Daria Vasilchenko <vasilchenkodd@gmail.com>
2021-12-28 14:35:51 +0300

    lab8

---
+++ lab8/my-app/src/components/FooterComp.js
@@ -0,0 +1,12 @@
+function FooterComp() {
+    return (
+        <div style={{bottom: '20px', position: 'absolute'}}>
+            <hr></hr>
+            <i>Группа:</i> ИУ5-52Б
+            <br></br>
+            <i>Студент:</i> Васильченко Дарья Дмитриевна
+        </div>
+    );
+}
+
+export default FooterComp;
